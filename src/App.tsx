2d3c5ed526b6f0051addd181a2206b457a959a9b
thecentinol <centinol@proton.me>
2026-01-23 04:23:18 +0200

    feat: add basic sidebar layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 // import { useState } from "react";
-import "./App.css";
+import Sidebar from "@/components/sidebar";
 
 function App() {
 	return (
-		<>
-			<div />
-		</>
+		<div className="flex w-screen h-screen bg-(--bg) p-2">
+			<Sidebar />
+			<div className="w-full h-full bg-(--fg) rounded-2xl overflow-y-auto"></div>
+		</div>
 	);
 }
 
---
+++ src/components/sidebar.tsx
@@ -0,0 +1,31 @@
+import { Ellipsis, PlusIcon, Settings } from "lucide-react";
+import { Button } from "@/components/ui/button";
+
+export default function Sidebar() {
+	return (
+		<div className="flex flex-col h-full w-[20%] bg-(--bg) text-(--text) pl-2 pr-3">
+			<h1 className="h-[5%]">SermoUI</h1>
+			<div className="h-[12%]">
+				<p className="text-(--text-muted) pl-1">Actions</p>
+				<Button className="justify-start w-full rounded-md hover:bg-(--fg-elevated) cursor-pointer p-1">
+					<PlusIcon />
+					New Chat
+				</Button>
+			</div>
+
+			<div className="h-full">
+				<p className="text-(--text-muted) pl-1">Chats</p>
+				<div className="overflow-y-auto">
+					<p className="flex items-center justify-between hover:bg-(--fg-hover) px-4 py-2 rounded-md">
+						Example
+						<Ellipsis className="cursor-pointer" />
+					</p>
+				</div>
+			</div>
+
+			<div className="h-[10%] border-t border-gray-500 flex items-center justify-end cursor-pointer">
+				<Settings />
+			</div>
+		</div>
+	);
+}
